a510233f7a456b70a291cde4dc68e2409976e2b1
André Alija Ramos Agostini <andrealijaramosagostini@gmail.com>
2025-01-28 17:04:17 -0300

    rotating Cube

--- js/app.js
+++ js/app.js
@@ -1,16 +1,18 @@
 var vertexShaderText = `
 precision mediump float;
-attribute vec2 vertPosition;
+attribute vec3 vertPosition;
 attribute vec3 vertColor;
+uniform mat4 mWorld;
+uniform mat4 mView;
+uniform mat4 mProj;
 
 varying vec3 fragColor;
 
 void main()
 {
     fragColor = vertColor;
-    gl_Position = vec4(vertPosition, 0.0, 1.0);
+    gl_Position = mProj * mView * mWorld * vec4(vertPosition, 1.0);
 }
-
 `
 
 var fragmentShaderText = `
@@ -46,6 +48,10 @@ function init_demo() {
 
     gl.clearColor(0.75, 0.85, 0.8, 1.0)
     gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+    
+    gl.enable(gl.DEPTH_TEST)
+    gl.enable(gl.CULL_FACE)
+    gl.cullFace(gl.Front)
 
     let vertexShader = gl.createShader(gl.VERTEX_SHADER);
     let fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
@@ -84,24 +90,83 @@ function init_demo() {
      //     Create Buffer
      //
 
-    let triangleVertices = 
-    [ // X,   Y         R   G   B
-        0.0, 0.5,      1.0, 0,  0,
-        -0.5, -0.5,     0, 1.0, 0,
-        0.5, -0.5,      0,  0, 1.0
-    ]  
-    let triangleVertexBufferObject = gl.createBuffer()
-    gl.bindBuffer(gl.ARRAY_BUFFER, triangleVertexBufferObject)
-    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(triangleVertices), gl.STATIC_DRAW)
+    let boxVertices = 
+    [ //  X,     Y,    Z        R      G      B
+        -1.0,   1.0,  -1.0,     0.50,   0.50,   0.50, // TOP
+        -1.0,   1.0,   1.0,     0.50,   0.50,   0.50,
+         1.0,   1.0,   1.0,     0.50,   0.50,   0.50,
+         1.0,   1.0,  -1.0,     0.50,   0.50,   0.50,
+         
+        -1.0,   1.0,   1.0,     0.75,   0.25,   0.5, // Left
+        -1.0,  -1.0,   1.0,     0.75,   0.25,   0.5,
+        -1.0,  -1.0,  -1.0,     0.75,   0.25,   0.5,
+        -1.0,   1.0,  -1.0,     0.75,   0.25,   0.5,
+        
+         1.0,   1.0,   1.0,     0.25,   0.25,   0.75, // Right
+         1.0,  -1.0,   1.0,     0.25,   0.25,   0.75,
+         1.0,  -1.0,  -1.0,     0.25,   0.25,   0.75,
+         1.0,   1.0,  -1.0,     0.25,   0.25,   0.75,
+        
+         1.0,   1.0,   1.0,     1.00,   0.00,   0.15, // Front
+         1.0,  -1.0,   1.0,     1.00,   0.00,   0.15,
+        -1.0,  -1.0,   1.0,     1.00,   0.00,   0.15,
+        -1.0,   1.0,   1.0,     1.00,   0.00,   0.15,
+        
+         1.0,   1.0,  -1.0,     1.00,   0.00,   0.15, // Back
+         1.0,  -1.0,  -1.0,     1.00,   0.00,   0.15,
+        -1.0,  -1.0,  -1.0,     1.00,   0.00,   0.15,
+        -1.0,   1.0,  -1.0,     1.00,   0.00,   0.15,
+
+        -1.0,  -1.0,  -1.0,     0.50,   0.50,   1.00, // Bottom
+        -1.0,  -1.0,   1.0,     0.50,   0.50,   1.00,
+         1.0,  -1.0,   1.0,     0.50,   0.50,   1.00,
+         1.0,  -1.0,  -1.0,     0.50,   0.50,   1.00,
+
+    ] 
+
+    let boxIndices = [
+        // TOP
+        0, 1, 2,
+        0, 2, 3,
+
+        // LEFT
+        5, 4, 6, 
+        6, 4, 7,
+
+        // RIGHT
+        8, 9, 10,
+        8, 10, 11,
+
+        // FRONT
+        13, 12, 14,
+        15, 14, 12,
+
+        // BACK
+        16, 17, 18,
+        16, 18, 19,
+
+        // BOTTOM
+        21, 20, 22,
+        22, 20, 23
+    ]
+
+    let BoxVertexBufferObject = gl.createBuffer()
+    gl.bindBuffer(gl.ARRAY_BUFFER, BoxVertexBufferObject)
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(boxVertices), gl.STATIC_DRAW)
+
+    let BoxIndexBufferObject = gl.createBuffer();
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, BoxIndexBufferObject)
+    gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(boxIndices), gl.STATIC_DRAW)
+
 
     let positionAttriLocation = gl.getAttribLocation(program, 'vertPosition')
     let colorAttriLocation = gl.getAttribLocation(program, 'vertColor')
     gl.vertexAttribPointer(
         positionAttriLocation, // Attribute Location
-        2, // Number of elements per attribute
+        3, // Number of elements per attribute
         gl.FLOAT, // type of elements,
         gl.FALSE, 
-        5 * Float32Array.BYTES_PER_ELEMENT, // Size of an individual Vertex -> 4
+        6 * Float32Array.BYTES_PER_ELEMENT, // Size of an individual Vertex -> 4
         0 // Offset from the beginning of a single vertex to this attribute
     )
     gl.vertexAttribPointer(
@@ -109,19 +174,57 @@ function init_demo() {
         3, // Number of elements per attribute
         gl.FLOAT, // type of elements,
         gl.FALSE, 
-        5 * Float32Array.BYTES_PER_ELEMENT, // Size of an individual Vertex -> 4
-        2 * Float32Array.BYTES_PER_ELEMENT,// Offset from the beginning of a single vertex to this attribute
+        6 * Float32Array.BYTES_PER_ELEMENT, // Size of an individual Vertex -> 4
+        3 * Float32Array.BYTES_PER_ELEMENT,// Offset from the beginning of a single vertex to this attribute
     )
 
     gl.enableVertexAttribArray(positionAttriLocation)
     gl.enableVertexAttribArray(colorAttriLocation)
+
+    // Tell OpenGL which program should be active 
+    gl.useProgram(program)
+
+    let matWorldUniformLocation         = gl.getUniformLocation(program, 'mWorld')
+    let matViewUniformLocation          = gl.getUniformLocation(program, 'mView')
+    let matProjectionUniformLocation    = gl.getUniformLocation(program, 'mProj')
+
+    let worldMatrix         = new Float32Array(16)
+    let viewMatrix          = new Float32Array(16)
+    let projectionMatrix    = new Float32Array(16)
+
+    glMatrix.mat4.identity(worldMatrix) 
+    glMatrix.mat4.lookAt(viewMatrix, [0, 0, -5], [0, 0, 0], [0, 1, 0])
+    glMatrix.mat4.perspective(projectionMatrix, glMatrix.glMatrix.toRadian(45), canvas.width / canvas.height, 0.1, 1000.0)
+
+    gl.uniformMatrix4fv(matWorldUniformLocation,        gl.FALSE, worldMatrix)
+    gl.uniformMatrix4fv(matViewUniformLocation,         gl.FALSE, viewMatrix)
+    gl.uniformMatrix4fv(matProjectionUniformLocation,   gl.FALSE, projectionMatrix)
     
     //
     // Main render loop
     //
-  
-    gl.useProgram(program)
-    gl.drawArrays(gl.TRIANGLES, 0, 3);
+
+    let identityMatrix = new Float32Array(16)
+    glMatrix.mat4.identity(identityMatrix)
+
+    let xRotationMatrix = new Float32Array(16)
+    let yRotationMatrix = new Float32Array(16)
+
+    function loop() {   // miliseconds / seconds/ 6 seconds/ 1 rotation each 6 seconds
+        var angle = performance.now() / 1000 / 6 * 2 * Math.PI
+        glMatrix.mat4.rotate(yRotationMatrix, identityMatrix, angle, [0, 1, 0])
+        glMatrix.mat4.rotate(xRotationMatrix, identityMatrix, angle / 4, [1, 0, 0])
+        glMatrix.mat4.mul(worldMatrix, yRotationMatrix, xRotationMatrix)
+        gl.uniformMatrix4fv(matWorldUniformLocation, gl.FALSE, worldMatrix)
+        
+        gl.clearColor(0.75, 0.85, 0.8, 1.0)
+        gl.clear(gl.DEPTH_BUFFER_BIT | gl.COLOR_BUFFER_BIT)
+        //gl.drawArrays(gl.TRIANGLES, 0, 3);
+        gl.drawElements(gl.TRIANGLES, boxIndices.length, gl.UNSIGNED_SHORT, 0)
+        requestAnimationFrame(loop)
+    }
+    requestAnimationFrame(loop)
+
 
 }
 
